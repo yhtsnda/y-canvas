8c55a36a778aaa330ab480312de1f846b4d1d2c4
RobbinZhu <robbin@59124.com>
2013-04-02 10:32:17 +0800

    Update Size.js
    update sizeEqual

--- Size.js
+++ Size.js
@@ -5,5 +5,5 @@ function SizeZero(){
 	return SizeMake(0,0);
 }
 function SizeEqual(sizeA,sizeB){
-	return sizeA.w === sizeB.w && sizeA.h === sizeB.h;
+	return sizeA === sizeB || (sizeA.w === sizeB.w && sizeA.h === sizeB.h);
 }
