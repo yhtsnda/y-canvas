d887fd59701c574cc285c92e8149cac0883ea449
Robbin.Zhu <robbin@59124.com>
2013-04-09 08:04:32 +0800

    Action.js

--- Action.js
+++ Action.js
@@ -1,9 +1,55 @@
 function Action(){
-	this.duration = null;
-	this.isDone = function(){};
+	this.duration = 0;
+	this.isDone = false;
 	this.exec('init');
 }
 Action.prototype = new BaseObject;
+
+Action.prototype.startWithTarget = function(target){
+    this.target = target;
+};
 Action.prototype.init = function(){
 	this.exec('onInit');
 };
+function MoveTo(){
+    Action.apply(this,arguments);
+    
+}
+function MoveBy(){
+    Action.apply(this,arguments);
+}
+function ScaleTo(){
+    Action.apply(this,arguments);
+}
+function ScaleBy(){
+    Action.apply(this,arguments);
+}
+function RotateTo(){
+    Action.apply(this,arguments);
+}
+function RotateBy(){
+    Action.apply(this,arguments);
+}
+function TintTo(){
+    Action.apply(this,arguments);
+}
+function TintBy(){
+    Action.apply(this,arguments);
+}
+function FadeTo(){
+    Action.apply(this,arguments);
+}
+function FadeBy(){
+    Action.apply(this,arguments);
+}
+function FadeIn(){
+    Action.apply(this,arguments);
+}
+function FadeOut(){
+    Action.apply(this,arguments);
+}
+function Sequence(){}
+function Repeat(){}
+function Spawn(){}
+function RepeatForever(){}
+function Delay(){}
