eb9c8a849fd3e8812e219edeace6718c30c9802e
Robbin Zhu <robbin.zhu@ef.com>
2013-04-11 17:57:37 +0800

    update action

--- Action.js
+++ Action.js
@@ -1,91 +1,213 @@
-function Action(){
-	this.duration = 0;
-	this.isDone = false;
+function Action(duration, callback) {
+    this.duration = duration;
+    this.isDone = false;
     this.elapsed = 0;
-    this.callback = null;
-	this.exec('init',arguments);
+    this.callback = callback;
+    this.init(arguments);
 }
 Action.prototype = new BaseObject;
-Action.prototype.step = function(dt){
-    this.elapsed = Math.min(this.duration,this.elapsed+dt);
-    if(this.elapsed >= this.duration){
+Action.prototype.step = function (dt) {
+    this.elapsed = Math.min(this.duration, this.elapsed + dt);
+    if (this.elapsed >= this.duration) {
         this.done();
-    }else{
+    } else {
         this.update();
     }
 };
-Action.prototype.done = function(){
+Action.prototype.done = function () {
     this.isDone = true;
-    if(this.callback){
+    if (this.callback) {
         this.callback();
         this.callback = null;
     }
 };
-Action.prototype.update = function(){
-    this.exec('onUpdate',arguments);
-    
-    this.exec('afterUpdate',arguments);
+Action.prototype.init = function () {
+    this.exec('onInit', arguments);
+    this.exec('_init', Array.prototype.slice.call(arguments, 2));
+    this.exec('afterInit', arguments);
 };
-Action.prototype.startWithTarget = function(target){
+Action.prototype.update = function () {
+    this.exec('onUpdate', arguments);
+    this.exec('_update', arguments);
+    this.exec('afterUpdate', arguments);
+};
+Action.prototype.startWithTarget = function (target) {
     this.target = target;
+    this.exec('_startWithTarget', arguments);
 };
-Action.prototype.init = function(){
-	this.exec('onInit',arguments);
-    this.exec('initWith',arguments);
-    this.exce('afterInit',arguments);
+function MoveTo() {
+    Action.apply(this, arguments);
+}
+MoveTo.prototype = new Action;
+MoveTo.prototype._init = function (toPosition) {
+    this.toPosition = toPosition;
 };
-function MoveTo(){
-    MoveTo.prototype.init.apply(this,arguments);
-}
-MoveTo.prototype = new Action();
-MoveTo.prototype.initWith = function(toPosition,duration,callback){
-    Action.apply(this,arguments);
-    this.toPosition = toPosition || PointZero();
-    this.duration = duration || 0;
-    this.callback = callback;
+MoveTo.prototype._startWithTarget = function () {
+    this.startPosition = this.target.position();
+    this.deltaPosition = PointDiff(this.toPosition, this.startPosition);
 };
-MoveTo.prototype.startWithTarget = function(){
-    Action.prototype.startWithTarget.apply(this,arguments);
-    this.deltaPosition = PointDiff(this.toPosition,this.target.position());
+MoveTo.prototype._update = function () {
+    this.target.position(PointSum(this.startPosition, this.deltaPosition * this.elapsed));
 };
-MoveTo.prototype.onUpdate = function(){
-    this.target.position(PointSum(this.target.position(),this.deltaPosition * this.elasped));
+function MoveBy() {
+    Action.apply(this, arguments);
+}
+MoveBy.prototype = new Action;
+MoveBy.prototype._init = function () {
+    MoveTo.apply(this, arguments);
 };
-function MoveBy(){
+MoveBy.prototype._startWithTarget = function () {
+    this.startPosition = this.target.position();
+    this.deltaPosition = this.toPosition;
+};
+MoveBy.prototype._update = function () {
+    MoveTo.prototype._update.apply(this, arguments);
+};
+function ScaleTo() {
+    Action.apply(this, arguments);
+}
+ScaleTo.prototype = new Action;
+ScaleTo.prototype._init = function (scaleTo) {
+    this.scaleTo = scaleTo;
+};
+ScaleTo.prototype._startWithTarget = function () {
+    this.startScale = this.target.scale();
+    this.deltaScale = PointDiff(this.scaleTo, this.startScale);
+};
+ScaleTo.prototype._update = function () {
+    this.target.scale(PointSum(this.startScale, this.deltaScale * this.elapsed));
+};
+function ScaleBy() {
+    Action.apply(this, arguments);
+}
+ScaleBy.prototype = new Action;
+ScaleBy.prototype._init = function () {
+    ScaleTo.prototype._init.apply(this, arguments);
+};
+ScaleBy.prototype._startWithTarget = function () {
+    this.startScale = this.target.scale();
+    this.deltaScale = this.scaleTo;
+};
+ScaleBy.prototype._update = function () {
+    ScaleTo.prototype._update.apply(this, arguments);
+};
+function RotateTo() {
+    Action.apply(this, arguments);
+}
+RotateTo.prototype = new Action;
+RotateTo.prototype._init = function (rotateTo) {
+    this.rotateTo = rotateTo;
+};
+RotateTo.prototype._startWithTarget = function () {
+    this.startRotate = this.target.rotate();
+    this.deltaRotate = PointDiff(this.rotateTo, this.startRotate);
+};
+RotateTo.prototype._update = function () {
+    this.target.rotate(PointSum(this.startRotate, this.deltaRotate * this.elapsed));
+};
+function RotateBy() {
+    Action.apply(this, arguments);
+}
+RotateBy.prototype = new Action;
+RotateBy.prototype._init = function () {
+    RotateTo.prototype._init.apply(this, arguments);
+};
+RotateBy.prototype._startWithTarget = function () {
+    this.startRotate = this.target.rotate();
+    this.deltaRotate = this.rotateTo;
+};
+RotateBy.prototype._update = function () {
+    RotateTo.prototype._update.apply(this, arguments);
+};
+function TintTo() {
+    Action.apply(this, arguments);
+}
+TintTo.prototype = new Action;
+TintTo.prototype._init = function (tintTo) {
+    this.tintTo = tintTo;
+};
+TintTo.prototype._startWithTarget = function () {
+    //TODO add tint support to Sprite
+    //this.startTint = this.target.tint();
+    //this.deltaTint =
+};
+TintTo.prototype._update = function () {
+    //TODO support update here
+};
+function TintBy() {
+    Action.apply(this, arguments);
+}
+TintBy.prototype = new Action;
+TintBy.prototype._init = function () {
+    TintTo.prototype._init.apply(this, arguments);
+};
+TintBy.prototype._startWithTarget = function () {
+    //TODO add tint support to Sprite
+    //this.startTint = this.target.tint();
+    //this.deltaTint =
+};
+TintBy.prototype._update = function () {
+    TintTo.prototype._update.apply(this, arguments);
+};
+function FadeTo() {
+    Action.apply(this, arguments);
+}
+FadeTo.prototype = new Action;
+FadeTo.prototype._init = function (fadeTo) {
+    this.fadeTo = fadeTo;
+};
+FadeTo.prototype._startWithTarget = function () {
+    this.startFade = this.target.alpha();
+    this.deltaFade = this.fadeTo - this.startFade;
+};
+FadeTo.prototype._update = function () {
+    this.target.alpha(this.startFade + this.deltaFade * this.elapsed);
+};
+function FadeBy() {
+    Action.apply(this, arguments);
+}
+FadeBy.prototype = new Action;
+FadeBy.prototype._init = function () {
+    FadeTo.prototype._init.apply(this, arguments);
+};
+FadeBy.prototype._startWithTarget = function () {
+    this.startFade = this.target.alpha();
+    this.deltaFade = this.fadeTo;
+};
+FadeBy.prototype._update = function () {
+    this.target.alpha(this.startFade + this.deltaFade * this.elapsed);
+};
+function FadeIn() {
+    Action.apply(this, arguments);
+}
+FadeIn.prototype = new Action;
+FadeIn.prototype._init = function () {
+    this.fadeTo = 1;
+};
+FadeIn.prototype._startWithTarget = function () {
+    FadeTo.prototype._startWithTarget.apply(this, arguments);
+};
+FadeIn.prototype._update = function () {
+    FadeTo.prototype._update.apply(this, arguments);
+};
+function FadeOut() {
+    Action.apply(this, arguments);
+}
+FadeOut.prototype._init = function () {
+    this.fadeTo = 0;
+};
+FadeOut.prototype._startWithTarget = function () {
+    FadeIn.prototype._startWithTarget.apply(this, arguments);
+};
+FadeOut.prototype._update = function () {
+    FadeIn.prototype._update.apply(this, arguments);
+};
+function Delay() {
     Action.apply(this,arguments);
 }
-function ScaleTo(){
-    Action.apply(this,arguments);
-}
-function ScaleBy(){
-    Action.apply(this,arguments);
-}
-function RotateTo(){
-    Action.apply(this,arguments);
-}
-function RotateBy(){
-    Action.apply(this,arguments);
-}
-function TintTo(){
-    Action.apply(this,arguments);
-}
-function TintBy(){
-    Action.apply(this,arguments);
-}
-function FadeTo(){
-    Action.apply(this,arguments);
-}
-function FadeBy(){
-    Action.apply(this,arguments);
-}
-function FadeIn(){
-    Action.apply(this,arguments);
-}
-function FadeOut(){
-    Action.apply(this,arguments);
-}
-function Sequence(){}
-function Repeat(){}
-function Spawn(){}
-function RepeatForever(){}
-function Delay(){}
+Delay.prototype = new Action;
+
+function Sequence() {}
+function Repeat() {}
+function Spawn() {}
+function RepeatForever() {}
--- Node.js
+++ Node.js
@@ -1,93 +1,116 @@
-function Node(){
-	this.position = (function(){
-		var pos = PointMake(0,0);
-		return function(position){
-			return position === undefined ? pos : pos = position;
-		}
-	})();
-	this.size = (function(){
-		var _size = SizeMake(0,0);
-		return function(size){
-			return size === undefined ? _size : _size = size;
-		};
-	})();
-	this.visible = (function(){
-		var _visible = true;
-		return function(visible){
-			return visible === undefined ? _visible : _visible = visible;
-		}
-	})();
-	this.images = (function(){
-		var _images = [];
-		return function(images){
-			return images === undefined ? _images : _images = images;
-		}
-	})();
-	this.imageIndex = 0;
-	this.zindex = (function(){
-		var _index = 0;
-		return function(index){
-			return index === undefined ? _index : _index = index;
-		}
-	});
-	this.anchor = (function(){
-		var _anchor = PointMake(0.5,0.5);
-		return function(anchor){
-			return anchor === undefined ? _anchor : _anchor = anchor;
-		};
-	})();
-	this.rotate = (function(){
-		var _rotate = 0;
-		return function(rotate){
-			return rotate === undefined ? _rotate : _rotate = rotate;
-		};
-	})();
-	this.scaleX = (function(){
-		var _scaleX = 1;
-		return function(scaleX){
-			return scaleX === undefined ? _scaleX : _scaleX = scaleX;
-		};
-	})();
-	this.scaleY = (function(){
-		var _scaleY = 1;
-		return function(scaleY){
-			return scaleY === undefined ? _scaleY : _scaleY = scaleY;
-		};
-	})();
-	this.skewX = (function(){
-		var _skewX = 0;
-		return function(skewX){
-			return skewX === undefined ? _skewX : _skewX = skewX;
-		};
-	})();
-	this.skewY = (function(){
-		var _skewY = 0;
-		return function(skewY){
-			return skewY === undefined ? _skewY : _skewY = skewY;
-		};
-	})();
-	this.transform = (function(){
-		var _transform = null;
-		return function(transform){
-			return transform === undefined ? _transform : _transform = transform;
-		};
-	})();
-    this.pause = (function(){
-        var _paused;
-        return function(paused){
-            return paused === undefined ? _paused: _paused = paused;
+function Node() {
+    this.position = function () {
+        var pos = PointMake(0, 0);
+        return function (position) {
+            return position === undefined ? pos : pos = position;
         }
-    })();
-    this.stop = function(){
+    }();
+    this.size = function () {
+        var _size = PointMake(0, 0);
+        return function (size) {
+            return size === undefined ? _size : _size = size;
+        }
+    }();
+    this.visible = function () {
+        var _visible = true;
+        return function (visible) {
+            return visible === undefined ? _visible : _visible = visible;
+        }
+    }();
+    this.images = function () {
+        var _images = [];
+        return function (images) {
+            return images === undefined ? _images : _images = images;
+        }
+    }();
+    this.imageIndex = function(){
+        var _index = 0;
+        return function(index){
+            return index === undefined ? _index : _index = index;
+        }
+    };
+    this.zIndex = function () {
+        var _index = 0;
+        return function (index) {
+            return index === undefined ? _index : _index = index;
+        }
+    }();
+    this.aplha = function(){
+        var _alpha = 0;
+        return function(alpha){
+            return alpha === undefined ? _alpha : _alpha = alpha;
+        }
+    }();
+    this.anchor = function () {
+        var _anchor = PointMake(0.5, 0.5);
+        return function (anchor) {
+            return anchor === undefined ? _anchor : _anchor = anchor;
+        }
+    }();
+    this.rotate = function () {
+        var _rotate = 0;
+        return function (rotate) {
+            return rotate === undefined ? _rotate : _rotate = rotate;
+        }
+    }();
+    this.scale = function () {
+        var _scale = PointMake(1, 1);
+        return function (scale) {
+            return scale === undefined ? _scale : _scale = scale;
+        }
+    }();
+    /*this.scaleX = function () {
+    var _scaleX = 1;
+    return function (scaleX) {
+    return scaleX === undefined ? _scaleX : _scaleX = scaleX;
+    };
+    }();
+    this.scaleY = function () {
+    var _scaleY = 1;
+    return function (scaleY) {
+    return scaleY === undefined ? _scaleY : _scaleY = scaleY;
+    };
+    }();*/
+    /*this.skewX = function () {
+    var _skewX = 0;
+    return function (skewX) {
+    return skewX === undefined ? _skewX : _skewX = skewX;
+    };
+    }();
+    this.skewY = function () {
+    var _skewY = 0;
+    return function (skewY) {
+    return skewY === undefined ? _skewY : _skewY = skewY;
+    };
+    }();*/
+    this.skew = function () {
+        var _skew = PointMake(1, 1);
+        return function (skew) {
+            return skew === undefined ? _skew ? _skew = skew;
+        }
+    }();
+    this.transform = function () {
+        var _transform = null;
+        return function (transform) {
+            return transform === undefined ? _transform : _transform = transform;
+        }
+    }();
+    this.pause = function () {
+        var _paused;
+        return function (paused) {
+            return paused === undefined ? _paused : _paused = paused;
+        }
+    }();
+    this.stop = function () {
         this.pause(true);
     };
-    this.resume = function(){
+    this.resume = function () {
         this.pause(false);
     };
-    this.children = (function(){
+    this.children = function () {
         var _children = [];
-        return function(children){
-            return children === undefined ? _children: _children = children;
+        return function (children) {
+            return children === undefined ? _children : _children = children;
         }
-    })();
+    }();
 }
