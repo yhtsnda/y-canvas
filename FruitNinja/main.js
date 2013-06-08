898251b37b7d8b7a46b4d6edf919297d817df22a
Robbin.Zhu <robbin@59124.com>
2013-06-08 19:55:50 +0800

    push

--- FruitNinja/main.js
+++ FruitNinja/main.js
@@ -184,12 +184,12 @@ function StartScene() {
         quit = new Sprite(asserts.quit),
         boom = new Sprite(asserts.startboom);
     mask.runAction(Easing.withAction(new MoveTo(new Point(0, 0), 1000), 'easeOutQuad'));
-    dojo.runAction(new Sequence(new Delay(2500), new ScaleTo(PointMake(1, 1), 1000), new RepeatForever(new RotateTo(-Math.PI * 2, 4000))));
-    game.runAction(new Sequence(new Delay(2500), new ScaleTo(PointMake(1, 1), 1000), new RepeatForever(new RotateTo(Math.PI * 2, 4000))));
-    quit.runAction(new Sequence(new Delay(2500), new ScaleTo(PointMake(1, 1), 1000), new RepeatForever(new RotateTo(-Math.PI * 2, 4000))));
-    sandia.runAction(new Sequence(new Delay(2500), new ScaleTo(PointMake(1, 1), 1000), new RepeatForever(new RotateTo(-Math.PI * 2, 4000))));
-    peach.runAction(new Sequence(new Delay(2500), new ScaleTo(PointMake(1, 1), 1000), new RepeatForever(new RotateTo(Math.PI * 2, 4000))));
-    boom.runAction(new Sequence(new Delay(2500), new ScaleTo(PointMake(1, 1), 1000), new RepeatForever(new RotateTo(Math.PI * 2, 4000))));
+    dojo.runAction(new Sequence(new Delay(2500), new ScaleTo(PointMake(1, 1), 300), new RepeatForever(new RotateTo(-Math.PI * 2, 4000))));
+    game.runAction(new Sequence(new Delay(2500), new ScaleTo(PointMake(1, 1), 300), new RepeatForever(new RotateTo(Math.PI * 2, 4000))));
+    quit.runAction(new Sequence(new Delay(2500), new ScaleTo(PointMake(1, 1), 300), new RepeatForever(new RotateTo(-Math.PI * 2, 4000))));
+    sandia.runAction(new Sequence(new Delay(2500), new ScaleTo(PointMake(1, 1), 300), new RepeatForever(new RotateTo(-Math.PI * 2, 4000))));
+    peach.runAction(new Sequence(new Delay(2500), new ScaleTo(PointMake(1, 1), 300), new RepeatForever(new RotateTo(Math.PI * 2, 4000))));
+    boom.runAction(new Sequence(new Delay(2500), new ScaleTo(PointMake(1, 1), 300), new RepeatForever(new RotateTo(Math.PI * 2, 4000))));
     desc.runAction(new Sequence(new Delay(2000), new MoveBy(PointMake(200, 0), 200)));
     ninja.runAction(new Sequence(new Delay(1000), Easing.withAction(new MoveBy(PointMake(0, 110), 1000), 'easeOutBounce')));
 
