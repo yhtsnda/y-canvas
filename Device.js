5ac6e5e5b81a71ca4278fe81a3cb71d83997be54
Robbin.Zhu <robbin@59124.com>
2013-06-29 18:25:24 +0800

    push

--- Device.js
+++ Device.js
@@ -1,13 +1,13 @@
-var device = {};
-domReady(function () {
-    device.resolution = SizeMake(document.body.clientWidth, document.body.clientHeight);
-    var dom = document.createElement('canvas');
-    dom.id = 'app';
-    document.body.appendChild(dom);
-    addEventHandler(window, 'resize', function (e) {
-        device.resolution.reset(document.body.clientWidth, document.body.clientHeight);
-    });
-});
+//var device = {};
+// domReady(function () {
+//     device.resolution = SizeMake(document.body.clientWidth, document.body.clientHeight);
+//     var dom = document.createElement('canvas');
+//     dom.id = 'app';
+//     document.body.appendChild(dom);
+//     addEventHandler(window, 'resize', function (e) {
+//         device.resolution.reset(document.body.clientWidth, document.body.clientHeight);
+//     });
+// });
 
 function disablePageMove(){
     addEventHandler(document,'touchstart',function(e){
