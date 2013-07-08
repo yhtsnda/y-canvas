192c64038a511042d05e12e998a72c6cdf05b169
Robbin.Zhu <robbin@59124.com>
2013-07-08 13:30:01 +0800

    push

--- Global.js
+++ Global.js
@@ -10,22 +10,46 @@ function addEventHandler(dom, type, fn) {
     }
 };
 
-function domReady(c) {
+function domReady(callback) {
     if (!("onreadystatechange" in document) || !("readystatechange" in document)) {
         var d = setTimeout(function() {
             clearTimeout(d);
             if ("complete" === document.readyState || "loaded" === document.readyState) {
-                c && c();
+                callback && callback();
             } else {
                 d = setTimeout(arguments.callee, 50);
             }
         }, 50);
     } else {
         addEventHandler(document, "readystatechange", function() {
-            ("complete" === this.readyState || "loaded" === this.readyState) && c && c();
+            ("complete" === this.readyState || "loaded" === this.readyState) && callback && callback();
         });
     }
 }
+
+function visibilityChange(callback) {
+    if ('hidden' in document) {
+        document.addEventListener('visibilitychange', function () {
+            callback(document.hidden);
+        }, false);
+    } else if ('webkitHidden' in document) {
+        document.addEventListener('webkitvisibilitychange', function () {
+            callback(document.webkitHidden);
+        }, false);
+    } else if ('mozHidden' in document) {
+        document.addEventListener('mozvisibilitychange', function () {
+            callback(document.mozHidden);
+        }, false);
+    } else if ('msHidden' in document) {
+        document.addEventListener('msvisibilitychange', function () {
+            callback(document.msHidden);
+        }, false);
+    } else {
+        return false;
+    }
+    return true;
+}
+
 var requestAnimFrame = (function() {
         return window.requestAnimationFrame ||
                window.webkitRequestAnimationFrame ||
