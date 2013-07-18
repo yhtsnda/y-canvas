2bcc24ed62f1d2c8bbf81d91026f669e5cc88a26
Robbin.Zhu <robbin@59124.com>
2013-07-18 13:13:44 +0800

    ,

--- EventSystem.js
+++ EventSystem.js
@@ -3,31 +3,30 @@ var EventSystem = {
     //mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave
     //change select submit keydown keypress keyup error contextmenu
     //touchstart touchmove touchend touchcancel
-    eventsType: [
-        ["mousedown", "mouseup", "mousemove", "mouseover", "mouseout", "mouseenter", "mouseleave"],
-        ["keydown", "keypress", "keyup"],
-        ["touchstart", "touchmove", "touchend", "touchcancel"]
-    ]
-};
-EventSystem.init = function() {
-    var MOUSEEVTINDEX = 0,
-        KEYBOARDEVTINDEX = 1,
-        TOUCHEVTINDEX = 2;
+    init: function() {
+        var me = this,
+            eventsType = [
+                ["mousedown", "mouseup", "mousemove", "mouseover", "mouseout", "mouseenter", "mouseleave"],
+                ["keydown", "keypress", "keyup"],
+                ["touchstart", "touchmove", "touchend", "touchcancel"]
+            ],
+            dom = getDom(),
+            handler = proxy(me.filterEvent, me);
 
-    var gainEvent = {
-        'mouse': function(e) {
-            this.events()[MOUSEEVTINDEX].some(function(event, index, originalE) {
-                if (event.type === e.type) {
-                    originalE = null;
-                }
+        eventsType.forEach(function(eventTypes) {
+            eventTypes.forEach(function(eventType) {
+                addEventHandler(dom, eventType, handler);
             });
-            this.events()[MOUSEEVTINDEX].removeNullVal();
+        });
+        this.resetEvents();
+    },
+    gainEvent: {
+        'mouse': function(e) {
             e.position = {
-                x: e.offsetX != null ? e.offsetX : e.pageX - e.target.offsetLeft,
-                y: e.offsetY != null ? e.offsetY : e.pageY - e.target.offsetTop
+                x: e.offsetX !== undefined ? e.offsetX : e.pageX - e.target.offsetLeft,
+                y: e.offsetY !== undefined ? e.offsetY : e.pageY - e.target.offsetTop
             };
-            //console.log(e.position);
-            this.events()[MOUSEEVTINDEX].push(e);
+            this.events()[0].push(e);
         },
         'key': function(e) {
             //var code = e.keyCode || e.which;
@@ -36,97 +35,72 @@ EventSystem.init = function() {
             Debugger.error('this function need to be implmented');
         },
         'touch': function(e) {
-            this.events()[TOUCHEVTINDEX].some(function(event, index, originalE) {
-                if (event.type === e.type) {
-                    originalE = null;
-                }
-            });
-            this.events()[TOUCHEVTINDEX].removeNullVal();
             var touch = e.touches[0];
             e.position = {
                 x: touch ? (touch.pageX - touch.target.offsetLeft) : 0,
                 y: touch ? (touch.pageY - touch.target.offsetTop) : 0
             };
-            this.events()[TOUCHEVTINDEX].push(e);
+            this.events()[2].push(e);
         }
-    };
-    var me = this;
-
-    function _gainEvent(e) {
-        if (!e || me.deallingEvents())
+    },
+    filterEvent: function(e) {
+        if (!e || this.deallingEvents())
             return;
-        for (var prop in gainEvent) {
-            if (e.type.indexOf(prop) == 0) {
-                gainEvent[prop].call(me, e);
-                break;
-            }
+        var match = e.type.match(/mouse|key|touch/);
+        if (match) {
+            this.gainEvent[match[0]].apply(this, arguments);
         }
+    },/*
+    removeEvent: function(e) {
+        this.events().some(function(eventsArray, index, events) {
+            return eventsArray.some(function(event, index, originalEventsArray) {
+                if (event === e) {
+                    originalEventsArray.splice(index, 1);
+                    return true;
+                }
+            });
+        });
+    },*/
+    events: prop([
+        [],
+        [],
+        []
+    ]),
+    deallingEvents: prop(),
+    resetEvents: function() {
+        this.events([
+            [],
+            [],
+            []
+        ]);
+        this.deallingEvents(false);
+    },
+    handleEventWithTarget: function(target) {
+        //mouse event
+        forEach(this.events()[0], function(event) {
+            var pos = target.actualPosition();
+            if (event.position.x >= pos.x &&
+                event.position.x <= pos.x + target.width() &&
+                event.position.y >= pos.y &&
+                event.position.y <= pos.y + target.height()) {
+                forEach(target['on' + event.type], function(handle) {
+                    handle.call(target, event);
+                });
+            }
+        });
+        //key event
+        forEach(this.events()[1], function(event) {});
+        //touch event
+        forEach(this.events()[2], function(event) {
+            var pos = target.actualPosition();
+            if (event.position.x >= pos.x &&
+                event.position.x <= pos.x + target.width() &&
+                event.position.y >= pos.y &&
+                event.position.y <= pos.y + target.height()) {
+                forEach(target['on' + event.type], function(handle) {
+                    handle.call(target, event);
+                });
+            }
+        });
     }
-
-    this.eventsType.forEach(function(eventTypes) {
-        eventTypes.forEach(function(eventType) {
-            addEventHandler(getDom(), eventType, _gainEvent);
-        });
-    });
-    this.resetEvents();
 };
-EventSystem.removeEvent = function(e) {
-    this.events().some(function(eventsArray, index, events) {
-        return eventsArray.some(function(event, index, originalEventsArray) {
-            if (event === e) {
-                originalEventsArray.splice(index, 1);
-                return true;
-            }
-        });
-    });
-};
-EventSystem.events = (function() {
-    var _events = [
-        [],
-        [],
-        []
-    ];
-    return function(events) {
-        return events === undefined ? _events : _events = events;
-    };
-})();
-EventSystem.deallingEvents = (function() {
-    var _inDealingEvents;
-    return function(inDealling) {
-        return inDealling === undefined ? _inDealingEvents : _inDealingEvents = inDealling;
-    };
-})();
-EventSystem.resetEvents = function() {
-    this.events([
-        [],
-        [],
-        []
-    ]);
-    this.deallingEvents(false);
-};
-EventSystem.handleEventWithTarget = function(target) {
-    //mouse event
-    forEach(EventSystem.events()[0], function(event) {
-        if (event.position.x >= target.actualPosition().x &&
-            event.position.x <= target.actualPosition().x + target.width() &&
-            event.position.y >= target.actualPosition().y &&
-            event.position.y <= target.actualPosition().y + target.height()) {
-            forEach(target['on' + event.type], function(handle) {
-                handle.call(target, event);
-            });
-        }
-    });
-    //key event
-    forEach(EventSystem.events()[1], function(event) {});
-    //touch event
-    forEach(EventSystem.events()[2], function(event) {
-        if (event.position.x >= target.actualPosition().x &&
-            event.position.x <= target.actualPosition().x + target.width() &&
-            event.position.y >= target.actualPosition().y &&
-            event.position.y <= target.actualPosition().y + target.height()) {
-            forEach(target['on' + event.type], function(handle) {
-                handle.call(target, event);
-            });
-        }
-    });
-}
