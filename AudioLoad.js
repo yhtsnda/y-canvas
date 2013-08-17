8cf69702fe6c553842b81422ff98432789b99098
Robbin.Zhu <robbin@59124.com>
2013-08-17 16:37:08 +0800

    audio fix

--- AudioLoad.js
+++ AudioLoad.js
@@ -2,7 +2,6 @@ function AudioLoad(src, callback) {
     var audio = document.createElement("audio");
     audio.autoplay = false;
     audio.preload = "auto";
-    audio.volumn = 0.00001;
     audio.src = src;
     addEventHandler(audio, "canplaythrough", function () {
         this.loaded = true;
