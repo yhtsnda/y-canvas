0bb0ce3fdb68cf7aee5b732a3f35e8ac5bda1032
RobbinZhu <robbin@59124.com>
2013-12-04 14:31:49 +0800

    Delete Debugger.js

--- Debugger.js
+++
@@ -1,9 +0,0 @@
-var Debugger = window.console || {
-    debug : function () {},
-    info : function () {},
-    warn : function () {},
-    assert : function () {},
-    error : function (msg) {
-        throw new Error(msg);
-    }
-};
